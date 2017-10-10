ed43cbe4c61735a4c9ff07203309212495cbc403
QA test <qatest@example.com>
2017-10-10 18:04:18 +0300

    hnjbvg

---
+++ example2.py
@@ -0,0 +1,15 @@
+a= int 9input()
+i = 0
+while i <= 10:
+    a, b = input().split()
+    a = int(a)
+    b = int(b)
+    if (a==0) and (b==0):
+        break
+    if (a==0) or (b==0):
+        continue
+    print(a*b)
+    i+=1
+
+else:
+    print('mjbh5')
