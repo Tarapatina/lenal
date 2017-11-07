5f54545ae2036d638b8bdb6817e7e6272760dcfd
QA test <qatest@example.com>
2017-11-07 18:04:59 +0200

    ok

--- stroki1.py
+++ stroki1.py
@@ -1,8 +1,8 @@
 students=['Ivan', 'Masha','Sasha']
-print (len (students))
 
-#for students in students:
-   # print('Hello,'+students+'!')
+
+for students in students:
+   print('Hello,'+students+'!')
 #teachers= ['Oleg','Alex']
 #print (students+teachers)
 #x=[0,1]*4
